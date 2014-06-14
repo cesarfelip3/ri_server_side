719fab0d78f57712dfee21e9c0e4038d6e07f111
hello <hello@hellotekiMac-mini.local>
2014-06-14 12:29:16 +0800

    update

--- src/Api/Controller/UserController.php
+++ src/Api/Controller/UserController.php
@@ -81,7 +81,7 @@ class UserController extends BaseController
             return $this->setFailed("Alarm time should not be empty");
         }
 
-        if ($alarm - $latency_end <= 0) {
+        if ($alarm - $latency_end <= 60) {
 
             return $this->setFailed("We are not able to send you remote notification #", array("latency_end"=>$latency_end, "latency_start"=>$latency_start, "alarm"=>$alarm));
 
